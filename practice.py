11235787e6e02c868dd9866f827880c69590dfb6
Eaglesflight <okonkwopaul348@gmail.com>
2026-08-17 11:58:15 +0100

    Add magician greetings and number statistics

--- practice.py
+++ practice.py
@@ -24,3 +24,35 @@ response = requests.get("https://api.github.com")
 data = response.json()
 print(data)
 print(data.get("current_user_url"))
+
+
+magicians = ["alice", "david", "caroline"]
+for guys in magicians:
+    print(f"{guys.title()}, that was a great trick!")
+    # print(f"I can't wait to see your next, {guys.title()}.\n")
+print(f"I can't wait to see your next trick, {guys.title()}.\n")
+
+
+#Using the range() Function
+for value in range(0,5):
+    print(value)
+
+
+#Using range() to Make a List of Numbers
+num = list(range(1,11,2))
+print(num)
+
+squares = []
+for value in range(1, 12):
+    squares.append(value **2)
+print(squares)
+
+# Simple Statistics with a List of Numbers
+digit = []
+for num in range(0,10):
+    digit.append(num)
+
+print(digit)
+print(max(digit))
+print(min(digit))
+print(sum(digit))

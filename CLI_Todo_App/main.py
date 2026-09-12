2639bb8cb909a8199168ddc30743b8d189a0c668
okonkwo348 <okonkwopaul348@gmail.com>
2026-09-12 23:10:11 +0100

    cli to-do-app

---
+++ CLI_Todo_App/main.py
@@ -0,0 +1,28 @@
+ class Task:
+
+    container = {}
+    def __int__(self):
+        self.title = title
+        self.done = done
+
+    def add_tasks(self.title):
+        container[self.title] = "not-done"
+        
+
+    def list_all_tasks(self.title, self.done):
+        return container
+
+    def mark_task(self.title, self.done):
+        container[self.title] = {self.done}
+    
+    def save():
+        with open("note.txt","w") as f:
+            f.write(key, val  for key, val in container)
+
+    def load():
+        pass
+
+
+    while True:
+        input("type the follow:\n 'add': to add a task\n 'list': to know the list of task\n 'done': to mark done to a task\n 'exit': to end the program")
+

d8dd9a9e5a07a986295bdbc49fe615d710ae1736
okonkwo348 <okonkwopaul348@gmail.com>
2026-09-13 23:31:42 +0100

    cli to-do-app upgrade

--- CLI_Todo_App/main.py
+++ CLI_Todo_App/main.py
@@ -4,15 +4,15 @@
     def __int__(self):
         self.title = title
         self.done = done
-
-    def add_tasks(self.title):
+    
+    def add_tasks(self, title):
         container[self.title] = "not-done"
         
 
-    def list_all_tasks(self.title, self.done):
+    def list_all_tasks(self, title, done):
         return container
 
-    def mark_task(self.title, self.done):
+    def mark_task(self, title, done):
         container[self.title] = {self.done}
     
     def save():
@@ -26,3 +26,4 @@
     while True:
         input("type the follow:\n 'add': to add a task\n 'list': to know the list of task\n 'done': to mark done to a task\n 'exit': to end the program")
 
+    

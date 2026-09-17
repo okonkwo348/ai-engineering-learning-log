d086c314734db5f277d6649e8e6b74daa54ec0b1
okonkwo348 <okonkwopaul348@gmail.com>
2026-09-17 14:24:42 +0100

    load_task function and error handling

--- CLI_Todo_App/main.py
+++ CLI_Todo_App/main.py
@@ -21,77 +21,105 @@
 #         container[self.title] = {self.done}
     
 #     def save():
-#         with open("note.txt","w") as f:
-#             f.write(key, val  for key, val in container)
+#         with open("note.txt","w") as file:
+#             file.write(key, val  for key, val in container)
 
 #     def load():
 #         pass
 
 
-#     while True:
-#         input("type the follow:\n 'add': to add a task\n 'list': to know the list of task\n 'done': to mark done to a task\n 'exit': to end the program")
+    # while True:
+    #     input("type the follow:\n 'add': to add a tasks\n 'list': to know the list of tasks\n 'done': to mark done to a tasks\n 'exit': to end the program")
 
     
 
-# import logging
+import logging
+import json
+from pathlib import Path
 
 
-# task = [
-#     {"title": "sweep", "done":False},
-#     {"title": "cook", "done":False},
-#     {"title": "brush", "done":False},
-#     {"title": "wash", "done":False},
-#     {"title": "read", "done":False},
-#     ]
+def load_task():
+    file_path = Path("file.json")
+
+    if file_path.is_file():
+        try:
+            with open("file.json", "r") as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+tasks = load_task()
 
 
-# def add_tasks(new_task):
-#     task.append({"title" :new_task, "done":False})
+def add_tasks(new_task):
+    tasks.append({"title" :new_task, "done":False})
 
-# def list_all_task():
-#     return task
+def list_all_task():
+    if tasks is None:
+            return f"Empty: add one tas...eg resting, cleaning"
+    else:
+        return tasks
 
-# def mark_task(index_list):
-#     task[index_list]["done"] = True
+def mark_task(index_list):
+    global tasks
+    if tasks is None:
+        tasks = []
+        tasks[index_list]["done"] = True
+    
 
-# def save_task():
-#     with open("file.json", "a") as f:
-#         for i in task: 
-#             f.write(str(i))
+def save_task():
+    with open("file.json", "w") as file:
+        json.dump(tasks, file, indent=2)
 
-# def load_task():
-#     with open("file.json", "r") as f:
-#         task = f.readlines()
-
-# logging.basicConfig(level = logging.INFO)
-# logger = logging.getLogger(__name__)
-# select = True
-# while select:
-#     load_task()
-#     print("Welcome to Todo App!")
-#     select =   input(": Enter: 'add' to add task, 'list' to list all tasks, 'done' to mark a task done, 'exit' to quite ")
-
-#     if select == "add":
-#         new_task = input(" Enter task e.g 'sweep', sleep'....")
-#         add_tasks(new_task)
-
-#     elif select == "list":
-#         print(list_all_task())
-
-#     elif select == "done":
-#         index_list = input("Enter the task no: ")
-#         try:
-#             mark_task(int(index_list))
-#         except IndexError as e:
-#             logger.error(f"Index of List out of range {e}")
-#         try:
-#             save_task()
-#         except FileNotFoundError as e:
-#             logger.error(f"File Not found {e}")
+    
 
 
-#     elif select == "exit":
-#         select = False
+        
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+select = True
+while select:
+    print("Welcome to Todo App!")
+    select =   input(": Enter: 'add' to add tasks, 'list' to list all tasks, 'done' to mark a tasks done, 'exit' to quite > ")
+    select = select.lower().strip()
+
+    if select == "add":
+        new_task = input(" Enter tasks e.g 'sweep', sleep'....>")
+        add_tasks(new_task)
+        save_task()
+
+    elif select == "list":
+        try:
+            load_task()
+        except FileNotFoundError as e:
+            logger.error(f"File Not found {e}")
+
+        print(list_all_task())
+
+    elif select == "done":
+        index_list = input("Enter the tasks no: ")
+        try:
+            load_task()
+        except FileNotFoundError as e:
+            logger.error(f"File Not found {e}")
+    
+        try:
+            mark_task(int(index_list))
+        except (ValueError, IndexError) as e:
+            logger.error(f"Index of List out of range {e}")
+        
+        try:
+            save_task()
+        except FileNotFoundError as e:
+            logger.error(f"File Not found {e}")
+
+            
+
+
+    elif select == "exit":
+        select = False
 
         
 

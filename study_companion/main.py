dc9d34a1be8cdd464896dda3de43817f0980548b
okonkwo348 <okonkwopaul348@gmail.com>
2026-09-21 15:32:26 +0100

    conversion of object memory with dunder mthods

--- study_companion/main.py
+++ study_companion/main.py
@@ -14,6 +14,14 @@ class QuizQuestion:
         """Rebuild a QuizzQuestion object from a plain dic (e.g. loaded from JSON)."""
         return cls(title=data["title"], question=data["question"], answer=data["answer"])
 
+    # __repr__  convert object memory address to unambiguous representation for developers, logging and debugging 
+    def __repr__(self):
+        """the string returned by __repr__ should look like valid python code that could recreate the object"""
+        return f"QuizQuestion(title={self.title!r}, question={self.question!r}, answer={self.answer!r})"
+
+
+    def __str__(self):
+        return f"[{self.title.upper()}] Q: {self.question} (Ans: {self.answer})"
 
 class StudySession:
 
@@ -30,6 +38,16 @@ class StudySession:
         questions_list = [QuizQuestion.from_dict(q) for q in data["questions"]]
         return cls(title=data["title"], summary=data["summary"], questions = questions_list)
 
+    ## __repr__  convert object memory address to unambiguous representation for developers, logging and debugging
+    def  __repr__(self):
+        """the string returned by __repr__ should look like valid python code that could recreate the object"""
+        return f"QuizQuestion(title={self.title!r}, summary={self.summary!r}, questions={self.questions!r})"
+
+    # __str__ convert object memory to clear, readable output meant for end user or UI dispay
+    def __str__(self):
+        """called automatically by print() or str()"""
+        return f"{[self.title.upper()]}, SUMMARY:{self.summary}, QUESTIONS:{self.questions}"
+
 
 num1 = QuizQuestion("variable", "is snakecase one of the way of declaring a variable", "True")
 num2 = QuizQuestion("class", "class name is declared using camelcase", "True")
@@ -41,6 +59,12 @@ print(session_dict)
 rebuild_session = StudySession.from_dict(session_dict)
 print(rebuild_session.questions[1].question)
 # print(new_session.questions[1].question)
+
+print(repr(num1))
+print(num1)
+
+print(repr(new_session))
+print(new_session)
 
 
 

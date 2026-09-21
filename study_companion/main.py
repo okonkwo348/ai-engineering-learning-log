3d900bdabaadaae859e119a48e60f28b6aff598e
okonkwo348 <okonkwopaul348@gmail.com>
2026-09-21 12:03:08 +0100

    object composition & serialization

--- study_companion/main.py
+++ study_companion/main.py
@@ -5,7 +5,7 @@ class QuizQuestion:
         self.question = question
         self.answer = answer
 
-    def to_dic(self) -> dict:
+    def to_dict(self) -> dict:
         """Convert this object into a plain, safe for json.jump()."""
         return {"title": self.title, "question": self.question, "answer": self.answer}
 
@@ -20,26 +20,27 @@ class StudySession:
     def __init__(self, title, summary, questions):
         self.title = title
         self.summary = summary
-        self.questions = QuizQuestion
+        self.questions = questions
 
-    def to_dic(self) -> dict:
-        return {"title":self.title, "summary":self.summary, "questions":json.dumps(self.questions)}
+    def to_dict(self) -> dict:
+        return {"title":self.title, "summary":self.summary, "questions":[question.to_dict() for question in self.questions]}
 
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(title=data["title"], summary=data["summary"], questions = data["questions"])
+        questions_list = [QuizQuestion.from_dict(q) for q in data["questions"]]
+        return cls(title=data["title"], summary=data["summary"], questions = questions_list)
 
 
 num1 = QuizQuestion("variable", "is snakecase one of the way of declaring a variable", "True")
 num2 = QuizQuestion("class", "class name is declared using camelcase", "True")
 
-print(num1.to_dic())
-print(num2.to_dic())
+new_session = StudySession("variable", "varibles are containers that store data values. They can store string, numbers, booleam, list etc", [num1,num2])
+session_dict = new_session.to_dict()
+print(session_dict)
 
-new_session = StudySession.from_dict(num1.to_dic)
-# session2 = StudySession.from_dict(num2.to_dic)
-
-print(new_session.questions[0].question)
+rebuild_session = StudySession.from_dict(session_dict)
+print(rebuild_session.questions[1].question)
+# print(new_session.questions[1].question)
 
 
 
